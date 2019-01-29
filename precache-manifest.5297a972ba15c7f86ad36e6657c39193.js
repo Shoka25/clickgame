889ef2c6a7062684cc37e8e1f2c6a01de4359922
John van der Hagen <johnmvanderhagen@gmail.com>
2019-01-29 03:11:03 -0600

    Updates

---
+++ precache-manifest.5297a972ba15c7f86ad36e6657c39193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickgame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1d1da0236ccd610ca49c",
+    "url": "/clickgame/static/js/runtime~main.1d1da023.js"
+  },
+  {
+    "revision": "7ece085d7b72cc36ded1",
+    "url": "/clickgame/static/js/main.7ece085d.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/clickgame/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "7ece085d7b72cc36ded1",
+    "url": "/clickgame/static/css/main.097f5bf6.chunk.css"
+  },
+  {
+    "revision": "d3aeed63775dea264f4163682e8ebe52",
+    "url": "/clickgame/index.html"
+  }
+];
